f94a9558b15c23da19dc2b1ce25cccc32bae3099
subharthi75 <subharthi75@gmail.com>
2024-04-13 16:19:56 +0530

    isActive

--- backend/routes/startup.js
+++ backend/routes/startup.js
@@ -2,8 +2,10 @@ const express = require("express");
 const createStartup = require("../controllers/createStartup");
 
 const { uploadFiles } = require("../utils/uploadFile");
+const isActiveStartup = require("../controllers/isActiveStartup");
 const router = express.Router();
 
 router.post("/create", uploadFiles, createStartup);
+router.get("/isactive/:useruid", isActiveStartup);
 
 module.exports = router;
