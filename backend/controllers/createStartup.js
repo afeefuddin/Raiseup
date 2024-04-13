709eebcf622afb2c9d5098d16cbbd6168e751567
subharthi75 <subharthi75@gmail.com>
2024-04-13 13:42:37 +0530

    startup creation added

---
+++ backend/controllers/createStartup.js
@@ -0,0 +1,47 @@
+const { Startup } = require("../model/Startup");
+const { User } = require("../model/User");
+
+async function createStartup(req, res) {
+  console.log(req.body);
+  const {
+    name,
+    description,
+    report,
+    stocks,
+    fundsRequired,
+    domain,
+    username,
+    logo,
+    websiteLink,
+    useruid,
+  } = req.body;
+
+  // console.log(req.body);
+
+  try {
+    const user = await User.findOne({ useruid });
+    if (user) {
+      const startup = new Startup({
+        name,
+        description,
+        report,
+        stocks,
+        fundsRequired,
+        domain: domain.split(","),
+        username,
+        logo,
+        websiteLink,
+        fundsRecieved: 0,
+        useruid: useruid,
+      });
+      await startup.save();
+      res.send(startup);
+    } else {
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+module.exports = createStartup;
---
+++ backend/models/Startup.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const startupSchema = new Schema({
+  useruid: { type: String, required: true, unique: true },
+
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  report: { type: String }, // URL of the report we will store in the cloudinary
+  fundsRecieved: { type: Number },
+  stocks: { type: Number },
+  fundsRequired: { type: Number },
+  domain: [{ type: String }],
+  founded: { type: Number, default: () => new Date().getFullYear() },
+  logo: { type: String, required: true }, // URL of the image
+  websiteLink: { type: String },
+  username: { type: String, required: true },
+});
+
+exports.Startup = mongoose.model("Startup", startupSchema);
