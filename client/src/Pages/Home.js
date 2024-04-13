94b2cf133530b76e20c11bd007b8dfb0f2ed4ac6
Afeefuddin <afeefud2004din@gmail.com>
2024-04-13 14:52:44 +0530

    HomePage completed

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,18 +2,40 @@ import React, { useState } from "react";
 import { useAuth } from "../Context/AuthContext";
 import Navbar from "../Components/ui/Navbar";
 import HeroSection from "../Components/sections/HeroSection";
+import Footer from "../Components/ui/Footer";
+import FeaturesSection from "../Components/sections/FeaturesSection";
+import TopStartups from "../Components/sections/TopStartups";
+import TeamsSection from "../Components/sections/TeamSection";
 
-const Home = () =>{
+const Home = () => {
     const { currentUser } = useAuth();
+    const page = currentUser ? "/dashboard" : "/signup";
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     const [activeLink, setActiveLink] = useState("home");
 
     const handleActiveLink = (link) => {
-      setActiveLink(link);
+        setActiveLink(link);
     };
     return (
         <>
-            <Navbar handleActiveLink={handleActiveLink} />
+            <Navbar
+                activeLink={activeLink}
+                isMenuOpen={isMenuOpen}
+                handleActiveLink={handleActiveLink}
+                handleMenuToggle={handleMenuToggle}
+                page={page}
+            />
             <HeroSection />
+            <FeaturesSection />
+            <TopStartups />
+            <TeamsSection />
+            <Footer />
         </>
     )
 
